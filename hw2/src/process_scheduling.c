e99823548888d5520424a2ccd7bdebb099c30eb3
sagecauley <sagecauley@ksu.edu>
2025-02-20 10:54:17 -0600

    got first come first serve

--- hw2/src/process_scheduling.c
+++ hw2/src/process_scheduling.c
@@ -20,15 +20,46 @@ void virtual_cpu(ProcessControlBlock_t *process_control_block)
 
 bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
-	UNUSED(ready_queue);
-	UNUSED(result);
-	return false;
+	if (ready_queue == NULL || result == NULL) {
+		return false;
+	}
+
+	int n = dyn_array_size(ready_queue);
+	if (n == 0) {
+		return false;
+	}
+
+	int current = 0;
+	int total_waiting = 0;
+	int total_running = 0;
+
+	for (size_t i = 0; i < n; i++) {
+		if (current < pcbs[i].arrival) {
+			current = pcbs[i].arrival;
+		}
+		void* dyn_array_at(const dyn_array_t* const dyn_array, const size_t index);
+		ProcessControlBlock_t *pcb = (ProcessControlBlock_t*)dyn_array_at(ready_queue, i);
+
+		int waiting_time = current_time - pcb->arrival;
+		total_waiting += waiting_time;
+
+		current += pcb->remaining_burst_time;
+
+		int running_time = current - pcb->arrival;
+
+		total_running += running_time;
+	}
+
+	result->average_waiting_time = (uint32_t)total_waiting / n;
+	result->average_turnaround_time = (uint32_t)total_running / n;
+	result->total_run_time = current_time;
+
+	return true;
 }
 
 bool shortest_job_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
-	UNUSED(ready_queue);
-	UNUSED(result);
+	//Need to use dyn_array_sort just not sure how atm
 	return false;
 }
 
