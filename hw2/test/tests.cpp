ddf1f70236dc84eed8d00b6c04f5f2db87d13631
jdcannell <jdcannell@ksu.edu>
2025-02-22 12:15:29 -0600

    Testing is hard

--- hw2/test/tests.cpp
+++ hw2/test/tests.cpp
@@ -35,10 +35,30 @@ class GradeEnvironment : public testing::Environment
 };
 
 
-
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	::testing::AddGlobalTestEnvironment(new GradeEnvironment);
 	return RUN_ALL_TESTS();
+}
+TEST(PCB, BadData)
+{
+	//Test
+	ASSERT_EQ(NULL, load_process_control_blocks(NULL));
+	ASSERT_EQ(NULL, load_process_control_blocks("ThisFileDoesNotExistHelloWorld"));
+}
+TEST(PCB, GoodData)
+{
+	dyn_array_t* ptr = load_process_control_blocks("pcb.bin");
+	ASSERT_NE((dyn_array_t*)NULL, ptr);
+
+	free(ptr);
+}
+TEST(FCFC, BadData)
+{
+	dyn_array_t * arPtr = dyn_array_create(4, sizeof(ProcessControlBlock_t), NULL);
+	ScheduleResult_t* schPtr = (ScheduleResult_t*)(sizeof(ScheduleResult_t));
+	ASSERT_EQ(false, first_come_first_serve(NULL, schPtr));
+	ASSERT_EQ(false, first_come_first_serve(arPtr, NULL));
+	//ASSERT_EQ(false, first_come_first_serve(arrPtr, NULL))
 }
