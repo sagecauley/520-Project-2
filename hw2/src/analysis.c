def5cb499e35181a9b1ea3f675989659d4e57b49
sagecauley <sagecauley@ksu.edu>
2025-02-22 17:37:19 -0600

    Maybe got Main

--- hw2/src/analysis.c
+++ hw2/src/analysis.c
@@ -19,7 +19,35 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	abort();		// REPLACE ME with implementation.
+	dyn_array_t* ready_queue = NULL;
 
+	FILE* file = fopen(argv[1], "r");
+	if (!file) {
+		return EXIT_FAILURE;
+	}
+
+	ready_queue = load_pcb_file(file);
+
+	if (!ready_queue || dyn_array_size(ready_queue) == 0) {
+		return EXIT_FAILURE;
+	}
+
+	char* algorithm = argv[2];
+	ScheduleResult_t result;
+
+	if (memcmp(algorithm, FCFS, 4) == 0) {
+		if (first_come_first_serve(ready_queue, &result))
+		{
+			printf("FCFS:\n");
+			printf("Average Waiting Time: %.2f\n", result.average_waiting_time);
+			printf("Average Turnaround Time: %.2f\n", result.average_turnaround_time);
+			printf("Total Run Time: %u\n", result.total_run_time);
+		}
+		else
+		{
+			printf("Error in FCFS scheduling\n");
+			return EXIT_FAILURE;
+		}
+	}
 	return EXIT_SUCCESS;
 }
